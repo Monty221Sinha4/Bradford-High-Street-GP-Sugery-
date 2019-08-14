6718249db604e1b0a61b2d74d8cdf78c58f8bb31
Monty Sinha <monty.sinha2013@live.co.uk>
2019-08-14 12:09:14 +0100

    Add files via upload

---
+++ Admin/UserLoginController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity.SqlServer;
+
+namespace GP_Project.Controllers.Admin
+{
+    public class UserLoginController : Controller
+    {
+        // GET: UserLogin
+        public ActionResult Login()
+        {
+            return View();
+        }
+       [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserLogin objuser)
+        {
+            if (ModelState.IsValid)
+            {
+                using(LoginEntities DB=new LoginEntities())
+                {
+                    var obj = DB.UserLogins.Where(a => a.Username.Equals(objuser.Username) && a.Password.Equals(objuser.Password)).FirstOrDefault();
+                    if(obj != null)
+                    {
+                        Session["UserID"] = obj.UserID.ToString();
+                        Session["UserName"] = obj.Username.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            }
+            return View(objuser);
+        }
+        public ActionResult Dashboard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else { return RedirectToAction("Login");
+            }
+        }
+    }
+}
